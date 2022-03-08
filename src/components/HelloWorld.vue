b0cf726f4352f33e10eac538f64f7a8cfa1cf661
Polo-Wolo <polo.walczak@gmail.com>
2022-03-08 09:01:34 +0100

    Install and testing FormKit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,64 +1,232 @@
-<script setup lang="ts">
-import { ref } from "vue";
-
-defineProps<{ msg: string }>();
-
-const count = ref(0);
-</script>
-
 <template>
-  <h1
-    class="bg-gradient-to-r from-green-600 to-sky-400 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent"
-  >
-    {{ msg }}
-  </h1>
+  <div class="container">
+    <div class="sample-input">
+      <h1>FormKit Starter Project</h1>
+      <FormKit type="group" v-model="formData">
+        <!-- Hello world -->
+        <FormKit
+          name="hello_world"
+          label="Hello World"
+          placeholder="Enter something here..."
+          validation="required"
+          validation-behavior="live"
+          help="Read the docs for more info on using FormKit"
+        />
+        <!-- Opinion - Radio choice -->
+        <FormKit
+          name="opinion"
+          label="Opinion"
+          help="How excited are you about FormKit?"
+          type="radio"
+          value="A lot"
+          :options="['A little', 'A lot']"
+        />
+        <!--File import -->
+        <FormKit
+          type="file"
+          label="Documents"
+          accept=".pdf,.doc,.docx,.xml,.md,.csv"
+          help="Select as many documents as you would like."
+          multiple
+        />
+        <!-- Color Picker -->
+        <FormKit
+          type="color"
+          value="#00FF00"
+          label="Select a color"
+          help="Select your favorite color."
+        />
+        <!-- Date -->
+        <FormKit
+          type="date"
+          value="2011-01-01"
+          label="Birthday"
+          help="Enter your birth day"
+          validation="required|before:2010-01-01"
+          validation-visibility="live"
+        />
+        <!-- Date Time -->
+        <FormKit
+          type="datetime-local"
+          value="2020-03-13T18:22"
+          label="End of the world"
+          help="When will the end of the world take place?"
+          validation="required|after"
+          validation-visibility="live"
+        />
+        <!-- Email -->
+        <FormKit
+          type="email"
+          label="Student email address"
+          help="Please enter your student email address."
+          validation="required|email|ends_with:.edu"
+          validation-visibility="live"
+          placeholder="vikas@school.edu"
+        />
+        <!-- Search -->
+        <FormKit
+          type="search"
+          placeholder="Search..."
+          label="Search"
+          value="Apple Cider"
+        />
+        <!-- Select -->
+        <FormKit
+          type="select"
+          label="What planet is the largest?"
+          placeholder="Select a planet"
+          name="planet"
+          :options="{
+            mercury: 'Mercury',
+            venus: 'Venus',
+            earth: 'Earth',
+            mars: 'Mars',
+            jupiter: 'Jupiter',
+            saturn: 'Saturn',
+            uranus: 'Uranus',
+            neptune: 'Neptune',
+          }"
+          validation="required|is:jupiter"
+          validation-visibility="dirty"
+          :validation-messages="{
+            is: 'Nope! Jupiter is the largest planet',
+          }"
+        />
+        <!-- Button + event -->
+        <FormKit
+          type="submit"
+          label="Click me!"
+          help="You can bind event listeners."
+          @click="PrintConsole"
+        ></FormKit>
+        <!-- Telephone -->
+        <FormKit
+          type="tel"
+          label="Phone number"
+          placeholder="xxx-xxx-xxxx"
+          validation="matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+          :validation-messages="{
+            matches: 'Phone number must be in the format xxx-xxx-xxxx',
+          }"
+          validation-visibility="dirty"
+        />
+        <!-- Text -->
+        <FormKit
+          type="text"
+          label="Your username"
+          value="calypso"
+          help="Pick a username people will remember!"
+        />
+        <!-- Text Area -->
+        <FormKit
+          type="textarea"
+          label="Your Essay"
+          rows="10"
+          placeholder="Remember to write in complete sentences."
+          help="I'll know if you didn't read the book!"
+        />
+        <!-- Time -->
+        <FormKit
+          type="time"
+          label="Time"
+          value="23:15"
+          help="What time will go home today?"
+        />
+        <!-- Url -->
+        <FormKit
+          type="url"
+          label="Favorite website"
+          placeholder="https://www.example.com..."
+          validation="required|url"
+          help="What is your favorite website?"
+        />
+        <!-- Week -->
+        <FormKit
+          label="Vacation"
+          type="week"
+          help="What week will you travel to Fiji?"
+          value="2025-W02"
+        />
+        <!-- Range bar -->
+        <div class="side-by-side">
+          <FormKit
+            name="recommendation"
+            label="Recommendation"
+            v-model="recommendation"
+            help="How likely are you to recommend FormKit to a friend?"
+            type="range"
+            min="0"
+            max="9"
+          />
+          <pre class="range-output">{{ recommendation }} </pre>
+        </div>
+      </FormKit>
 
-  <p class="mt-6 space-x-4">
-    Recommended IDE setup:
-    <a
-      href="https://code.visualstudio.com/"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-      >VSCode</a
-    >
-    +
-    <a
-      href="https://github.com/johnsoncodehk/volar"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-      >Volar</a
-    >
-  </p>
-
-  <p class="mt-4 space-x-4">
-    <a
-      href="https://vitejs.dev/guide/features.html"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-    >
-      Vite Documentation
-    </a>
-    |
-    <a
-      href="https://v3.vuejs.org/"
-      class="text-emerald-500 underline transition-all hover:text-emerald-400"
-      target="_blank"
-      >Vue 3 Documentation</a
-    >
-  </p>
-
-  <button
-    type="button"
-    class="my-6 rounded bg-gray-300 px-4 py-2 transition-all hover:bg-gray-200"
-    @click="count++"
-  >
-    count is: {{ count }}
-  </button>
-  <p>
-    Edit
-    <code class="text-gray-400">components/HelloWorld.vue</code> to test hot
-    module replacement.
-  </p>
+      <h2>Modeled group values</h2>
+      <pre class="form-data">{{ formData }}</pre>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const recommendation = ref("9");
+    const formData = ref({});
+
+    return {
+      recommendation,
+      formData,
+    };
+  },
+  methods: {
+    PrintConsole: function () {
+      console.log("Button click");
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  margin: auto;
+  display: flex;
+  justify-content: center;
+}
+
+.side-by-side {
+  display: flex;
+  align-items: flex-start;
+}
+
+pre.range-output {
+  background: #eee;
+  border-radius: 0.5em;
+  text-align: center;
+  margin-left: 1em;
+  margin-top: 1.5em;
+  font-weight: bold;
+  padding: 0.5em;
+  line-height: 1;
+  width: 1.5em;
+}
+
+pre.form-data {
+  box-sizing: border-box;
+  background: #eee;
+  border: 1px solid #ccc;
+  width: 100%;
+  padding: 1em;
+  border-radius: 0.5em;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { plugin, defaultConfig } from "@formkit/vue";
+import "@formkit/themes/genesis";
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(plugin, defaultConfig).mount("#app");
